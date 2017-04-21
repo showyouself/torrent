ebfa42e0886d0921f50568566b4b96d35a5dba7f
zengbin <200854748@qq.com>
2017-04-21 18:39:48 +0800

    x

--- app/Home/Model/FileListModel.class.php
+++ app/Home/Model/FileListModel.class.php
@@ -15,12 +15,26 @@ class  FileListModel extends BaseModel
 
 	public function updateFileList($magnet_id, $data)
 	{
-		$old = $this->file_list_tbl->where(array('magnet_id' => $magnet_i))->find();	
+		$where = array('magnet_id' => $magnet_i);
+		$old = $this->file_list_tbl->where($where)->find();	
 		if (empty($old)) {
-			$data = array(
-					'data' => $data,
-					);
+			$da = array('data' => $data,);
+			$this->file_list_tbl->where($where)->data($da)->add();
 		}
 	}
 
+	private function encode($data)
+	{
+		if (isset($data['data'])) { $data['data'] = json_encode($data['data']); }
+
+		return $data;
+	}
+
+	private function decode($data)
+	{
+		if (isset($data['data']) AND !empty($data['data'])) { $data['data'] = json_decode($data['data'], true); }
+		else { $data['data'] = array(); }
+
+		return $data;
+	}
 }
